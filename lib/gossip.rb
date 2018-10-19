435c258c6a63788b64d43e0a05e585f5a3d9b45d
CapellutoY <yossi.capelluto@gmail.com>
2018-10-19 16:57:39 +0200

    gossip update

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -1,9 +1,12 @@
 class Gossip
   attr_reader :author,:content
   def initialize(author, content)
-  @content = content
-  @author = author
-end
+    @content = content
+    @author = author
+  end
+  def save
+    gossip_to_csv = Hash.new { |@author, @content| @author[@content] =  "#{@author}: #{@content}" }
+  end
 end
 my_gossip = Gossip.new(author, content)
 my_gossip.save
