b2a004c5a419b567812403d360533baa1389dcfb
Antoine Jessen <antoinejessen@hotmail.com>
2018-10-19 17:03:06 +0200

    conflict

--- lib/gossip.rb
+++ lib/gossip.rb
@@ -5,15 +5,8 @@ class Gossip
     @author = author
   end
   def save
-<<<<<<< HEAD
-    my_gossip = Gossip.new(author, content)
-    my_gossip.save
-  end
-end
-=======
     gossip_to_csv = Hash.new { |@author, @content| @author[@content] =  "#{@author}: #{@content}" }
   end
 end
 my_gossip = Gossip.new(author, content)
 my_gossip.save
->>>>>>> 435c258c6a63788b64d43e0a05e585f5a3d9b45d
